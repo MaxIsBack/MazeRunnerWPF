b8097e6232f8c398ce7359c76ffebd44a5379ea1
Austen <austensaffron@gmail.com>
2019-11-19 08:44:57 -0800

    thinking about how to handle asking questions.

---
+++ WpfApp2/Maze/Trebek.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeRunnerWPF
+{
+    class Trebek
+    {
+
+      public void AskQuestion(int questionIndex)
+        {
+
+            Question theQuestion = GamePlay._TheMaze.GetQuestion(questionIndex);
+
+            // gui knows how to display question. Trebek jst gets input and lets the gui if the player got the question right.
+            //and then updates the question status.
+
+            string input = Gui.getInput();
+            
+            //validateinput
+
+
+            if (correct)
+            theQuestion.Unlock;
+
+        }
+
+    }
+}
