ec9c3b8646d8656a6ef6ed0d89002f8945072ed4
Austen <austensaffron@gmail.com>
2019-11-19 08:52:26 -0800

    ...

--- WpfApp2/Maze/Trebek.cs
+++ WpfApp2/Maze/Trebek.cs
@@ -9,21 +9,39 @@ namespace MazeRunnerWPF
     class Trebek
     {
 
-      public void AskQuestion(int questionIndex)
+        public static bool AskQuestion(int questionIndex)
         {
 
             Question theQuestion = GamePlay._TheMaze.GetQuestion(questionIndex);
 
             // gui knows how to display question. Trebek jst gets input and lets the gui if the player got the question right.
             //and then updates the question status.
+            
+
+            while (!inputValid) { }
 
             string input = Gui.getInput();
-            
+
             //validateinput
+
+            
+
+        }
+
+        //check answer
+
+
+
 
 
             if (correct)
-            theQuestion.Unlock;
+            {
+                theQuestion.Unlock;
+                return true;
+            }
+            else
+                return false;
+                    
 
         }
 
