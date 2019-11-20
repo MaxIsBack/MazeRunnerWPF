0452661bc62203feef67940c306301311326fb7b
Hawsoo <Hawsoo@users.noreply.github.com>
2019-11-19 22:36:09 -0800

    Adds look left and right async animation

--- WpfApp2/MazeGui.xaml.cs
+++ WpfApp2/MazeGui.xaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,15 +16,74 @@ using System.Windows.Shapes;
 
 namespace MazeRunnerWPF
 {
-        /// <summary>
-        /// Interaction logic for MainWindow.xaml
-        /// </summary>
-        public partial class MainWindow : Window
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
         {
-            public MainWindow()
+            InitializeComponent();
+            CurrentAngle = targetAngle = GetLookRotation();
+        }
+
+        private void btnTurnLeft_Click(object sender, RoutedEventArgs e) { TurnLeft(); }
+        private void btnTurnRight_Click(object sender, RoutedEventArgs e) { TurnRight(); }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left) TurnLeft();
+            else if (e.Key == Key.Right) TurnRight();
+        }
+
+
+
+
+        private void TurnLeft()
+        {
+            Turn(-90);
+        }
+
+        private void TurnRight()
+        {
+            Turn(90);
+        }
+
+        private double targetAngle;
+        private void Turn(double angle)
+        {
+            targetAngle += angle;
+            CurrentAngle = GetLookRotation();
+            new Thread(new ThreadStart(TurnAnimateAsync)).Start();
+        }
+
+        private delegate void UpdateSetLookRotation(double angle);
+        private void SetLookRotation(double angle)
+        {
+            lookRotation.Angle = angle;
+        }
+
+        private double GetLookRotation()
+        {
+            return lookRotation.Angle;
+        }
+
+        private double CurrentAngle;
+        private void TurnAnimateAsync()
+        {
+            int ticks = 30;
+            double turnDelta = (targetAngle - CurrentAngle) / ticks;
+            for (int i = 0; i < ticks; i++)
             {
-                InitializeComponent();
+                Thread.Sleep(1000 / 60);
+                CurrentAngle += turnDelta;
+                Console.WriteLine(CurrentAngle);
+                Dispatcher.Invoke(
+                    new UpdateSetLookRotation(this.SetLookRotation),
+                    new object[] { CurrentAngle }
+                );
             }
         }
+    }
 }
 
